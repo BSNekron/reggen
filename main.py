1f89429c2d2e063aa830f6d1241b1102b004571f
nekro <nekron_bs@bk.ru>
2021-08-30 22:34:17 +0300

    v1.3

--- main.py
+++ main.py
@@ -2,23 +2,27 @@ from datetime import datetime
 import pandas as pd
 import openpyxl
 
+sumpatp = 7720489
+sumbtu = 1954511
+
 
 def write_str(t_id, c_id):
     dfp = df.loc[df['tid'].isin(t_id)]
     for i in dfp.index:
         string = str(c_id) + ';0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
             df['priсe'][i]) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
+        with open('c:/worker/' + regname, 'a') as f_in:
+            f_in.write(string + "\n")
 
 
-
-sumpatp = 7720489
-sumbtu = 1954511
+regname = 'RegisterSubscriptionSales_00032_00046_1000000001_' + str(datetime.today().strftime("%Y%m$d")) + '.csv'
 summ = 0
 fsell = 'C:/worker/sell.csv'
 fpart = 'C:/worker/part.xlsx'
 df = pd.read_csv(fsell, delimiter=',', header=0, dtype={'price': int})
 dfpart = pd.read_excel(fpart, header=0, engine='openpyxl')
-
+with open('c:/worker/' + regname, 'a') as f_in:
+    f_in.write("OrganizationID;INN;BIC;MifareUID;TransportCardID;TicketID;Amount;Datetime\n")
 write_str([49, 45, 173, 47], 112)
 write_str([174, 44, 46, 48], 113)
 
@@ -30,16 +34,24 @@ for i in dfp.index:
             string = '112;0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
                 df['priсe'][i]) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
             print(string)
+            with open('c:/worker/' + regname, 'a') as f_in:
+                f_in.write(string + "\n")
         elif summ > sumpatp:
             price = summ - sumpatp
             string = '112;0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
-            df['priсe'][i] - price) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
+                df['priсe'][i] - price) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
             print(string)
+            with open('c:/worker/' + regname, 'a') as f_in:
+                f_in.write(string + "\n")
             string = '113;0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
-            price) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
+                price) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
             print(string)
-            summ=sumpatp
+            with open('c:/worker/' + regname, 'a') as f_in:
+                f_in.write(string + "\n")
+            summ = sumpatp
     else:
         string = '113;0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
             df['priсe'][i]) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
         print(string)
+        with open('c:/worker/' + regname, 'a') as f_in:
+            f_in.write(string + "\n")
