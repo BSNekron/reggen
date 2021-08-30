21abe77c62cf59efdb68967168944bb2b01cbe2b
nekro <nekron_bs@bk.ru>
2021-08-30 21:38:04 +0300

    v1.2

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from datetime import datetime
 import pandas as pd
 import openpyxl
+
+
+def write_str(t_id, c_id):
+    dfp = df.loc[df['tid'].isin(t_id)]
+    for i in dfp.index:
+        string = str(c_id) + ';0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
+            df['priсe'][i]) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
+        print(string)
+
+
 fsell = 'C:/worker/sell.csv'
 fpart = 'C:/worker/part.xlsx'
 df = pd.read_csv(fsell, delimiter=',', header=0, dtype={'price': int})
-dfp = df.loc[df['tid'].isin([49, 45, 173, 47])]
 dfpart = pd.read_excel(fpart, header=0, engine='openpyxl')
-print(dfp.index)
-amountnow =0
-for i in dfp.index:
-         string = '112;0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
-            df['priсe'][i]) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
-         print(string)
+amountnow = 0
+write_str([49, 45, 173, 47], 112)
+write_str([174, 44, 46, 48], 113)
+dfp = df.loc[df['tid'].isin([34, 28, 50, 33])]
+
