233dd7eeb1954890966fb8d99b27279b0e5bf3fa
BSNekron <nekron_bs@bk.ru>
2021-09-02 14:23:12 +0300

    2.0

--- reggenPRO.py
+++ reggenPRO.py
@@ -16,15 +16,15 @@ prev_tid = 0
 with open('c:/worker/' + regname, 'a') as f_in:
     f_in.write("OrganizationID;INN;BIC;MifareUID;TransportCardID;TicketID;Amount;Datetime\n")
 for t in dfpart.index:
-    print(str(dfpart['type'][t]))
+    print(str(dfpart['type'][t] + ' : ' + dfpart['company_id'][t]))
     t_id = str(dfpart['type'][t])
     c_id = str(dfpart['company_id'][t])
     dfp = df.loc[df['type_id'].isin([t_id])]
     dfp = dfp.reset_index()
-    print(dfp)
-    if prev_tid != t_id:
-        cur_sum = 0
-    for i in range(start, dfp.index.max(),1):
+    if int(t_id) != prev_tid:
+        start = 0
+        part = 0
+    for i in range(start, dfp.index.max(), 1):
         dfreg = pd.read_csv('c:/worker/' + regname, header=0, dtype={'amount': int}, delimiter=';')
         if dfreg.empty:
             cur_sum = 0
@@ -33,23 +33,27 @@ for t in dfpart.index:
             dfcomp = dfcomp.loc[dfcomp['TicketID'].isin([t_id])]
             cur_sum = dfcomp['Amount'].sum()
         amount = dfpart['amount'][t]
-        cur_sum = cur_sum + df['priсe'][i]
+        cur_sum = cur_sum + dfp['priсe'][i]
         if cur_sum <= amount:
             if part != 0:
                 string = str(c_id) + ';0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
                     part) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
             else:
                 string = str(c_id) + ';0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
-                    df['priсe'][i]) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
+                    dfp['priсe'][i]) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
             with open('c:/worker/' + regname, 'a') as f_in:
                 f_in.write(string + "\n")
             part = 0
         elif cur_sum > amount:
             part = cur_sum - amount
             string = str(c_id) + ';0;0;1;' + str(dfp['card'][i]) + ';' + str(dfp['type_id'][i]) + ';' + str(
-                df['priсe'][i] - part) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
+                dfp['priсe'][i] - part) + ';' + str(datetime.today().strftime("%d.%m.%Y %H:%M:%S"))
             with open('c:/worker/' + regname, 'a') as f_in:
                 f_in.write(string + "\n")
             start = i
             prev_tid = dfp['type_id'][i]
+            dfreg = pd.read_csv('c:/worker/' + regname, header=0, dtype={'amount': int}, delimiter=';')
+            dfcomp = dfreg.loc[dfreg['OrganizationID'].isin([c_id])]
+            dfcomp = dfcomp.loc[dfcomp['TicketID'].isin([t_id])]
+            print(dfcomp['Amount'].sum())
             break
